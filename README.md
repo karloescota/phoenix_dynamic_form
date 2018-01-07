11cc2962d84b8b9eb38ca31c2092ac5c89bde268
Karlo Escota <karlo.escota@sephoradigital.com>
2018-01-07 13:33:51 +0800

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+# Phoenix Dynamic Form
+
+From http://blog.plataformatec.com.br/2016/09/dynamic-forms-with-phoenix/.
+Reference so I don't keep on copying this for every new phoenix app
+
+## Instructions
+- add `import YourApp.InputHelpers` under `view` of `lib/app_web.ex` file
+- copy `input_helpers.ex` under `lib/app_web/views` folder
+- use inside form `<%= input f, :name %>`
---
+++ input_helpers.ex
@@ -0,0 +1,26 @@
+defmodule AppWeb.InputHelpers do
+  use Phoenix.HTML
+
+  def input(form, field) do
+    type = input_type(form, field)
+
+    wrapper_opts = [class: "form-group #{state_class(form, field)}"]
+    label_opts = [class: "control-label"]
+    input_opts = [class: "form-control"]
+
+    content_tag :div, wrapper_opts do
+      label = label(form, field, humanize(field), label_opts)
+      input = apply(Phoenix.HTML.Form, type, [form, field, input_opts])
+      error = AppWeb.ErrorHelpers.error_tag(form, field)
+      [label, input, error || ""]
+    end
+  end
+
+  defp state_class(form, field) do
+    cond do
+      !form.source.action -> ""
+      form.errors[field] -> "has-error"
+      true -> ""
+    end
+  end
+end
